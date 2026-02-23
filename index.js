1499f5df099af939fa3a30b26ca862deef43a9c6
Muh. Faja Alaudin Azmi <88223709+Fajaalaudin@users.noreply.github.com>
2026-02-23 19:39:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,3 +38,9 @@ async function checkWeatherAndMarket() {
 
 setInterval(checkWeatherAndMarket, 120000);
 checkWeatherAndMarket();
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.write("Bot is running");
+  res.end();
+}).listen(process.env.PORT || 3000);
